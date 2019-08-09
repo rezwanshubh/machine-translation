297b5317bc90109c6b56b316f2b3d9f8e25befff
rezwanshubh <shubhbd@yahoo.com>
2019-08-09 18:27:18 +0300

    preparing files for parser.

--- rnn-plus/preprocess.py
+++ rnn-plus/preprocess.py
@@ -4,7 +4,14 @@ def main():
     print('test')
 
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('train_data')
+    arg_parser.add_argument('-train_data_src', required=True)
+    arg_parser.add_argument('-train_data_tgt', required=True)
+    arg_parser.add_argument('-valid_data_src', required=True)
+    arg_parser.add_argument('-valid_data_tgt', required=True)
+    arg_parser.add_argument('-vocab_data_src', required=True)
+    arg_parser.add_argument('-vocab_data_tgt', required=True)
+    arg_parser.add_argument('-save_all_data', required=True)
+    arg_parser.add_argument('-max_len', '--max_word_seq_len', type=int, default=50)
 
 if __name__ == '__main__':
 	main()
