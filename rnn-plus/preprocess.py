cc21234cc93f38597acea06e2b5ffa3133323518
rezwanshubh <shubhbd@yahoo.com>
2019-08-17 00:37:26 +0300

    ready data preprocess.

--- rnn-plus/preprocess.py
+++ rnn-plus/preprocess.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import torch
 
 def read_instances_from_file(inst_file, max_sent_len):
     word_insts = []
@@ -23,6 +24,21 @@ def read_instances_from_file(inst_file, max_sent_len):
               .format(trimmed_sent_count, max_sent_len))
 
     return word_insts
+
+def convert_instance_to_idx_seq(word_insts, word2idx):
+    idx_seq_arr = []
+    for instance in word_insts:
+        idx_seq = []
+        for token in instance:
+            try:
+                idx_seq.append(word2idx[token])
+            except:
+                idx_seq.append(word2idx['<UNK>'])
+        idx_seq_arr.append(idx_seq)
+
+    return idx_seq_arr
+
+
 
 def main():
     print('test')
@@ -48,7 +64,7 @@ def main():
         print('warn: If training instance count is not equal!')
         quit()
 
-    #validation set
+    # validation set
     valid_src_word_instance = read_instances_from_file(opt.valid_src, opt.max_word_seq_len)
     valid_tgt_word_instance = read_instances_from_file(opt.valid_tgt, opt.max_word_seq_len)
 
@@ -56,12 +72,34 @@ def main():
         print('warn: If validation instance count is not equal')
         quit()
 
-    #vocabulary
-    src_word_tO_idx = json.load(open(opt.src_vocab)) # inter data exchange
+    # vocabulary
+    src_word_to_idx = json.load(open(opt.src_vocab)) # inter data exchange
     tgt_word_to_idx = json.load(open(opt.tgt_vocab))
 
+    # word to index
+    print('info: convert source word to sequence of words')
+    train_src_instance = convert_instance_to_idx_seq(train_src_word_inststance, src_word_to_idx)
+    valid_src_instance = convert_instance_to_idx_seq(valid_src_word_instance, src_word_to_idx)
 
+    print('info: convert target word to sequence of words.')
+    train_tgt_instance = convert_instance_to_idx_seq(train_tgt_word_inststance, tgt_word_to_idx)
+    valid_tgt_instance = convert_instance_to_idx_seq(valid_tgt_word_instance, tgt_word_to_idx)
 
+    data = {
+        'settings': opt,
+        'dict': {
+            'src': src_word_to_idx,
+            'tgt': tgt_word_to_idx},
+        'train': {
+            'src': train_src_instance,
+            'tgt': train_tgt_instance},
+        'valid': {
+            'src': valid_src_instance,
+            'tgt': valid_tgt_instance}}
+
+    print('info: save all processed data into a file', opt.save_data)
+    torch.save(data, opt.save_data)
+    print('---done---')
 
 if __name__ == '__main__':
 	main()
