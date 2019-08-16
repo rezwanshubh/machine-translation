7896e464443d38b0c6f11f83900a14e6c46c372f
rezwanshubh <shubhbd@yahoo.com>
2019-08-16 22:55:27 +0300

    validation set, vocabulary ready.

--- rnn-plus/preprocess.py
+++ rnn-plus/preprocess.py
@@ -1,4 +1,28 @@
 import argparse
+import json
+
+def read_instances_from_file(inst_file, max_sent_len):
+    word_insts = []
+    trimmed_sent_count = 0
+    with open(inst_file) as f:
+        for sent in f:
+            words = sent.split()
+            if len(words) > max_sent_len:
+                trimmed_sent_count += 1
+            word_inst = words[:max_sent_len]
+
+            if word_inst:
+                word_insts += [['<BOS>'] + word_inst + ['<EOS>']]
+            else:
+                word_insts += [None]
+
+    print('[Info] Get {} instances from {}'.format(len(word_insts), inst_file))
+
+    if trimmed_sent_count > 0:
+        print('[Warning] {} instances are trimmed to the max sentence length {}.'
+              .format(trimmed_sent_count, max_sent_len))
+
+    return word_insts
 
 def main():
     print('test')
@@ -12,6 +36,32 @@ def main():
     arg_parser.add_argument('-vocab_data_tgt', required=True)
     arg_parser.add_argument('-save_all_data', required=True)
     arg_parser.add_argument('-max_len', '--max_word_seq_len', type=int, default=50)
+
+    opt = arg_parser.parse_args()
+    opt.max_token_seq_len = opt.max_word_seq_len + 2    #test
+
+    # Training set
+    train_src_word_inststance = read_instances_from_file(opt.train_src, opt.max_word_seq_len)
+    train_tgt_word_inststance = read_instances_from_file(opt.train_tgt, opt.max_word_seq_len)
+
+    if len(train_src_word_inststance) != len(train_tgt_word_inststance):
+        print('warn: If training instance count is not equal!')
+        quit()
+
+    #validation set
+    valid_src_word_instance = read_instances_from_file(opt.valid_src, opt.max_word_seq_len)
+    valid_tgt_word_instance = read_instances_from_file(opt.valid_tgt, opt.max_word_seq_len)
+
+    if len(valid_src_word_instance) != len(valid_tgt_word_instance):
+        print('warn: If validation instance count is not equal')
+        quit()
+
+    #vocabulary
+    src_word_tO_idx = json.load(open(opt.src_vocab)) # inter data exchange
+    tgt_word_to_idx = json.load(open(opt.tgt_vocab))
+
+
+
 
 if __name__ == '__main__':
 	main()
