b4afde7ed6d4d2327cf996a7fe5f21e939ec5f38
rezwanshubh <shubhbd@yahoo.com>
2019-09-01 15:49:40 +0300

    update preprocess

--- test_bash_10.sh
+++ test_bash_10.sh
@@ -1,11 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+#SBATCH --job-name=rez_10
+#SBATCH --partition=gpu
+#SBATCH --ntasks=1
+#SBATCH --time=7-
+#SBATCH --cpus=1
+#SBATCH --mem-per-cpu=12000
+#SBATCH --gres=gpu:tesla:1
+module unload cudnn-6.0
+module load cudnn-5.1
 
-python ./rnn-plus/preprocess.py
-    -train_src ../dataset/en.train.enc \
-    -train_tgt ../dataset/et.train.enc \
-    -valid_src ../dataset/en.dev.enc \
-    -valid_tgt ../dataset/et.dev.enc \
-    -src_vocab ../dataset/sp.en.vocab \
-    -tgt_vocab ../dataset/sp.et.vocab \
-    -save_data ./saved_preprocessed_data_10 \
-    -max_len 50
+python ./rnn-plus/preprocess.py \
+-train_src ../dataset/en.train.enc \
+-train_tgt ../dataset/et.train.enc \
+-valid_src ../dataset/en.dev.enc \
+-valid_tgt ../dataset/et.dev.enc \
+-src_vocab ../dataset/en.bpe.train.json \
+-tgt_vocab ../dataset/et.bpe.train.json \
+-save_data ./saved_preprocessed_data_10.pt \
+-max_len 50
