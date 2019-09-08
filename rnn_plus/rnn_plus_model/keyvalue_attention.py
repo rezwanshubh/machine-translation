e8de273a3e4d180e445795f2c0216d917b285733
rezwanshubh <shubhbd@yahoo.com>
2019-09-08 23:09:16 +0300

    update comment.

--- rnn_plus/rnn_plus_model/keyvalue_attention.py
+++ rnn_plus/rnn_plus_model/keyvalue_attention.py
@@ -8,15 +8,24 @@ import torch.nn.functional as F
 class KeyValAttention(nn.Module):
 
     def __init__(self, scaling=False, dropout_ratio=0):
+        """Initialize a key-value attention class.
+        Args:
+            scaling - Whether normalize the attention weights by sqrt(size)
+            dropout_ratio - The probability of dropout on the logits
+        """
         super(KeyValAttention, self).__init__()
         self._scaling = scaling
         self._dropout = nn.Dropout(dropout_ratio) if dropout_ratio > 0 else None
 
     def forward_2d(self, query, keys, values, mask=None, additional_logits=None):
+        """Compute attention for 2-dimensional queries (batch x hidden).
+        """
         context_vector, weights = self.forward_3d(query.unsqueeze(-2), keys, values, mask, additional_logits)
         return context_vector.squeeze(-2), weights.squeeze(-2)
 
     def forward_3d(self, query, keys, values, mask=None, additional_logits=None):
+        """Compute attention for 3-dimensional input (batch x step x hidden).
+        """
         logits = torch.matmul(query, keys.transpose(-2, -1))
         if additional_logits is not None:
             logits += additional_logits
@@ -29,6 +38,7 @@ class KeyValAttention(nn.Module):
                 mask = mask.unsqueeze(-2)
             logits = logits.masked_fill(mask == 0, -1e9)
         elif self._dropout is not None:
+            # Using dropout but no mask
             mask = self._dropout(logits.new_ones(logits.shape))
             logits = logits.masked_fill(mask == 0, -1e9)
         weights = F.softmax(logits, dim=-1)
@@ -36,6 +46,11 @@ class KeyValAttention(nn.Module):
         return context_vector, weights
 
     def forward(self, query, keys, values, mask=None, additional_logits=None):
+        """Compute the context vector with key value attention.
+
+        Returns:
+            context vector and attention weights.
+        """
         if query.dim() == keys.dim() - 1:
             return self.forward_2d(query, keys, values, mask, additional_logits=additional_logits)
         else:
