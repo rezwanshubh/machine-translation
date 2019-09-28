799fc419135b06212ceee86571255f79b9463e61
Rezwanul Islam <rezwanul.islam@arvato.com>
2019-09-29 01:43:00 +0300

    gitignore added.

---
+++ transformer/run_preprocess.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+python ./OpenNMT-py/preprocess.py \
+-train_src data/src-train.txt \
+-train_tgt data/tgt-train.txt \
+-valid_src data/src-val.txt \
+-valid_tgt data/tgt-val.txt \
+-save_data data/demo
