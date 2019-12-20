dae00d5f90bde5eab1a551edd96171499f424ccf
Rezwanul Islam <Rezwanul.Islam@arvato.com>
2019-12-20 16:36:13 +0200

    Update encoder decoder.

--- rnmt_plus/rnmt_plus_model/layer.py
+++ rnmt_plus/rnmt_plus_model/layer.py
@@ -1,12 +1,11 @@
 from torch import nn
-from rnmt_plus_model.relational_rnn import RelationalRNN
-
+from rnmt_plus_model.relational_rnn import RelationalRNN_module
 
 class EncoderLayer(nn.Module):
 
     def __init__(self, d_embed, d_hidden):
         super(EncoderLayer, self).__init__()
-        self.encoder_gru = RelationalRNN(d_embed, d_hidden, batch_first=True, bidirectional=True)
+        self.encoder_gru = RelationalRNN_module(d_embed, d_hidden, batch_first=True, bidirectional=True)
 
     def forward(self, enc_input):
         enc_states, _ = self.encoder_gru(enc_input)
@@ -17,7 +16,7 @@ class DecoderLayer(nn.Module):
 
     def __init__(self, d_embed, d_hidden):
         super(DecoderLayer, self).__init__()
-        self.decoder_gru = RelationalRNN(d_embed, d_hidden, batch_first=True)
+        self.decoder_gru = RelationalRNN_module(d_embed, d_hidden, batch_first=True)
 
     def forward(self, dec_input, dec_init_state):
         dec_states, dec_last_hidden = self.decoder_gru(dec_input, dec_init_state)
