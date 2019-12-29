81324a1d246c98037328329fda273387ad2d1cde
Rezwanul Islam <Rezwanul.Islam@arvato.com>
2019-12-29 22:46:58 +0200

    Update

--- rnmt_plus/rnmt_plus_model/layer.py
+++ rnmt_plus/rnmt_plus_model/layer.py
@@ -5,7 +5,7 @@ class EncoderLayer(nn.Module):
 
     def __init__(self, d_embed, d_hidden):
         super(EncoderLayer, self).__init__()
-        self.encoder_relrnn = RelationalRNN_module(d_embed, d_hidden)
+        self.encoder_relrnn = RelationalRNN_module(input_size = 200,mem_slots=1, head_size =int(2500/4),num_heads=4,gate_style='memory',attention_mlp_layers=5,dropout_p=0.3)
 
     def forward(self, enc_input):
         enc_states, _ = self.encoder_relrnn(enc_input)
@@ -16,7 +16,7 @@ class DecoderLayer(nn.Module):
 
     def __init__(self, d_embed, d_hidden):
         super(DecoderLayer, self).__init__()
-        self.decoder_relrnn = RelationalRNN_module(d_embed, d_hidden)
+        self.decoder_relrnn = RelationalRNN_module(input_size = 200,mem_slots=1, head_size =int(2500/4),num_heads=4,gate_style='memory',attention_mlp_layers=5,dropout_p=0.3)
 
     def forward(self, dec_input, dec_init_state):
         dec_states, dec_last_hidden = self.decoder_relrnn(dec_input, dec_init_state)
