8e7ea557b9b63e502c7bd09c9e0e4d1989f94ca5
Rezwanul Islam <Rezwanul.Islam@arvato.com>
2020-05-02 15:34:59 +0600

    Update float to str conversion

--- evaluation/run_evaluation.py
+++ evaluation/run_evaluation.py
@@ -5,19 +5,19 @@ domain= 'domain-movie-subtitle'
 reference = open(domain + "/et.txt", "r", encoding="utf8").read()
 hypothesis = open(domain + "/google_et.txt", "r", encoding="utf8").read()
 BLEUscore = nltk.translate.bleu_score.sentence_bleu([reference], hypothesis)
-print('Google Trnaslator: ' + BLEUscore)
+print('Google Trnaslator: ' + str(BLEUscore))
 
 reference = open(domain + "/et.txt", "r", encoding="utf8").read()
 hypothesis = open(domain + "/rnmt_plus_et.txt", "r", encoding="utf8").read()
 BLEUscore = nltk.translate.bleu_score.sentence_bleu([reference], hypothesis)
-print('RNMT+ with Relational-RNN: ' +BLEUscore)
+print('RNMT+ with Relational-RNN: ' + str(BLEUscore))
 
 reference = open(domain + "/et.txt", "r", encoding="utf8").read()
 hypothesis = open(domain + "/sequence_et.txt", "r", encoding="utf8").read()
 BLEUscore = nltk.translate.bleu_score.sentence_bleu([reference], hypothesis)
-print('Sequence to Sequence with Attention and pyTorch: ' + BLEUscore)
+print('Sequence to Sequence with Attention and pyTorch: ' + str(BLEUscore))
 
 reference = open(domain + "/et.txt", "r", encoding="utf8").read()
 hypothesis = open(domain + "/transformer_et.txt", "r", encoding="utf8").read()
 BLEUscore = nltk.translate.bleu_score.sentence_bleu([reference], hypothesis)
-print('Transformer: ' + BLEUscore)
+print('Transformer: ' + str(BLEUscore))
