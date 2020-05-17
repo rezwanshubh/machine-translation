096c8399551e46461ab93094ccb2f3e556943446
Rezwanul Islam <Rezwanul.Islam@arvato.com>
2020-05-17 20:23:56 +0600

    Update scripts

--- transformer/translate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-python src_enc.py
-
-python ./OpenNMT-py/translate.py -model ./models/_step_350000.pt -src ./en.enc.txt -output ./et.enc.txt -replace_unk -verbose
-
-python tgt_dec.py
