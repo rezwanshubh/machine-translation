08cedb9d0123d84b8805711a3c2e5c3e830c317d
Rezwanul Islam <Rezwanul.Islam@arvato.com>
2020-05-17 20:24:06 +0600

    Update scripts

--- transformer/run_preprocess.sh
+++ transformer/run_preprocess.sh
@@ -1,14 +1,15 @@
-#!/usr/bin/env bash
-#SBATCH --job-name=rez_transformer
+#!/bin/bash
+#
+#SBATCH --job-name=train_model
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:tesla:1
 #SBATCH --time=7-
 #SBATCH --cpus=1
 #SBATCH --mem-per-cpu=120000
 
-python ./OpenNMT-py/preprocess.py \
--train_src data/src-train.txt \
--train_tgt data/tgt-train.txt \
--valid_src data/src-val.txt \
--valid_tgt data/tgt-val.txt \
--save_data data/demo
+python ./preprocess.py \
+-train_src ./en.train.enc \
+-train_tgt ./et.train.enc \
+-valid_src ./en.dev.enc \
+-valid_tgt ./et.dev.enc \
+-save_data ./data
