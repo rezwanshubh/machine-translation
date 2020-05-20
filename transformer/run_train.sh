6b0c5f7b2acbbc9f9616069b4b42d8db287cea1f
Rezwanul Islam <Rezwanul.Islam@arvato.com>
2020-05-20 19:46:20 +0600

    Update script, remove params.

--- transformer/run_train.sh
+++ transformer/run_train.sh
@@ -6,12 +6,9 @@
 #SBATCH --cpus=1
 #SBATCH --mem-per-cpu=120000
 
-python ./OpenNMT-py/train.py -data ./dataset/data -save_model ./models/ \
-        -layers 6 -rnn_size 512 -word_vec_size 512 -transformer_ff 2048 -heads 8  \
-        -encoder_type transformer -decoder_type transformer -position_encoding \
-        -train_steps 350000  -max_generator_batches 2 -dropout 0.1 \
-        -batch_size 4096 -batch_type tokens -normalization tokens  -accum_count 2 \
-        -optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 8000 -learning_rate 2 \
-        -max_grad_norm 0 -param_init 0  -param_init_glorot \
-        -label_smoothing 0.1 -valid_steps 10000 -save_checkpoint_steps 10000 \
-        -world_size 1 -gpu_ranks 0
+python ./OpenNMT-py/train.py -data ./data -save_model ./models/ \
+  -layers 6 -rnn_size 512 -word_vec_size 512 -transformer_ff 2048 \
+  -heads 8 -encoder_type transformer -decoder_type transformer \
+  -position_encoding -train_steps 350000 -dropout 0.1 -batch_size 4096 \
+  -gpu_ranks 0
+
